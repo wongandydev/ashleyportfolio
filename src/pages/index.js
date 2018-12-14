540d6c92c101ac110e70774a906bca739f7ccefd
Andy Wong <wongandy97@gmail.com>
2018-12-14 13:00:40 -0500

    add content to webpage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,26 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
+import './index.css'
+
 import Layout from '../components/layout'
 import ReasonToLiveImage from '../images/reasonstolive.jpg'
+import DrawingPortfolio from '../images/hina.jpg'
 
 const IndexPage = () => (
-  <Layout>
-    <h1>This will be Ashley's Site</h1>
+  <Layout>    
+    <div id="content">
+      <div id="reasonstolive">
+        <a target='_blank' href='https://www.wattpad.com/story/1624528-reasons-to-live'><img src={ReasonToLiveImage} alt="Reason's To Live, Ashley's Book" /></a>
+        <div> This is Ashley's first book. She started writing Reasons to Live __ years ago.</div>
+      </div>
+      <div id="drawing">
+        <a target='_blank' href='https://www.instagram.com/pyonanimates/'><img src={DrawingPortfolio} alt="This is Ashley's Instagram, where she she shows her drawing" /></a> 
+        <div> This is Ashley's portfolio. Here is one of her drawings, click on the image to go to her instagram and see more! </div>
+      </div>
+    </div>
     
-    <img src={ReasonToLiveImage} alt="a book" />
-
-    <div> Ashley is a creatitve writer and artist.</div>
-    <Link to="/page-2/">Go to page 2</Link>
+    {/* <Link id='linktopage2' to="/page-2/">Go to page 2</Link> */}
   </Layout>
 )
 
